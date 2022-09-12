01b4c7d4335e1d034b2adb361c0322be5a5829bb
miss-mad <mmlh.harris@gmail.com>
2022-09-11 20:52:49 -0400

    more setup

---
+++ index.js
@@ -0,0 +1,5 @@
+// "require" is a way to target a file or package needed to create this readme generator app
+// in the first two cases, "require" allows us to use certain packages within this js file
+const inquirer = require("inquirer");
+const fs = require("fs");
+
